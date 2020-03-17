2b4441b0e47dc147e5528f1819794acff3b0ccc4
devma <TheLippyT>
2020-03-17 03:08:32 -0300

    Rest API kinda works. packages updated. main activity intent has been updated.

--- app/src/main/java/com/example/notanotherworkoutapp/MainActivity.java
+++ app/src/main/java/com/example/notanotherworkoutapp/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
     {
         @Override
         public void onClick (View v){
-        Intent a = new Intent(MainActivity.this, MainActivity.class);
+        Intent a = new Intent(MainActivity.this, EditWorkAct.class);
         a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
             startActivity(a);
 
