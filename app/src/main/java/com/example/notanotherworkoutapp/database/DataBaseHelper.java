2b3bf40412eab7a2b110bf604069a6f19b056a59
devma <TheLippyT>
2020-03-19 07:48:30 -0300

    Changed the database extension.

--- app/src/main/java/com/example/notanotherworkoutapp/database/DataBaseHelper.java
+++ app/src/main/java/com/example/notanotherworkoutapp/database/DataBaseHelper.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
-    public  static  final String DATABASE_NAME = "fitapp";
+    public  static  final String DATABASE_NAME = "fitapp.db";
     private static final int DATABASE_VERSION = 1;
     public  static  final String TABLE_WORKOUTS = "workouts";
     public  static  final String TABLE_WORKOUTS_EXERCISE = "workouts_exercise";
