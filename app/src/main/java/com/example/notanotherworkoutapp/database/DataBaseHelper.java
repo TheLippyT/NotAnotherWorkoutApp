a58f5b9d40c8b5c2976047bb44e0f05f5adaaf29
Enverny Iengibe <envernyiengibe@gmail.com>
2020-03-19 21:30:52 -0300

    DATABASE IS NU COMPLEET MET CHECKUPS, LISTS ENZ

--- app/src/main/java/com/example/notanotherworkoutapp/database/DataBaseHelper.java
+++ app/src/main/java/com/example/notanotherworkoutapp/database/DataBaseHelper.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
-    public  static  final String DATABASE_NAME = "fitapp.db";
+    public  static  final String DATABASE_NAME = "fitapplication.db";
     private static final int DATABASE_VERSION = 1;
     public  static  final String TABLE_USERS = "users";
     public  static  final String TABLE_EXERCISE = "exercises";
